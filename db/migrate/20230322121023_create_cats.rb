1b99c2d830125cded5a8e9f7158ccb785effd22d
LisaDaudibon <lisa.daudibon@yahoo.fr>
2023-03-22 13:15:34 +0100

    migration base

--- db/migrate/20230322121023_create_cats.rb
+++ db/migrate/20230322121023_create_cats.rb
@@ -1,6 +1,9 @@
 class CreateCats < ActiveRecord::Migration[7.0]
   def change
     create_table :cats do |t|
+      t.string :name
+      t.text :description
+      t.integer :age
 
       t.timestamps
     end
